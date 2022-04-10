b4fa6cd9f3501c90ed2e1dca9ac65935379acfdf
Cristopher Namchee <cristophernamchee12@gmail.com>
2022-04-10 14:31:04 +0700

    debug: log when parsing dictionary

--- internal/repository/dict.go
+++ internal/repository/dict.go
@@ -31,6 +31,8 @@ func parseDictionaryFile(fsys fs.FS, filename string) map[string]string {
 	var data map[string]string
 	err = json.NewDecoder(file).Decode(&data)
 
+	fmt.Println(data)
+
 	if err != nil {
 		return map[string]string{}
 	}
